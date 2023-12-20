6257df646980ee7535db76318c8dab590f9eecb6
Ankit Pal <38988649+palankit003@users.noreply.github.com>
2023-12-20 14:36:33 +0530

    Initial commit: Setup the basic NextApp with Header component

---
+++ app/components/Header.tsx
@@ -0,0 +1,14 @@
+export default function Header() {
+    return (
+        <header>
+            <nav>
+                <ul>
+                    <li>Home</li>
+                    <li>Projects</li>
+                    <li>Blog</li>
+                    <li>About</li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
