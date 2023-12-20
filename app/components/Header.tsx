7d7441b2d056585fdbb6122ee147dc85f7c4c746
Ankit Pal <38988649+palankit003@users.noreply.github.com>
2023-12-20 23:29:09 +0530

    WIP: done some modification in navbar, need more touch

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,12 +1,15 @@
 export default function Header() {
     return (
         <header>
-            <nav>
-                <ul>
-                    <li>Home</li>
-                    <li>Projects</li>
-                    <li>Blog</li>
-                    <li>About</li>
+            <nav className="flex items-center mt-5
+                            bg-[#ffffffb2] [backdrop-filter: blur(10px)] p-2.5 rounded-md
+           ">
+                <span className=" text-4xl font-bold mr-2.5">Ankit</span>
+                <ul className="flex">
+                    <li className="m-5">Home</li>
+                    <li className="m-5">Projects</li>
+                    <li className="m-5">Blog</li>
+                    <li className="m-5">About</li>
                 </ul>
             </nav>
         </header>
