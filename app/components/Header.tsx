c8bc99226b3defd2177e3553b22e8fcb0d1c9b16
Ankit Pal <38988649+palankit003@users.noreply.github.com>
2023-12-21 22:13:20 +0530

    Modify: Add bars below nav links & add top margin to nav ul

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,15 +1,25 @@
 export default function Header() {
     return (
         <header>
-            <nav className="flex items-center mt-5
-                            bg-[#ffffffb2] [backdrop-filter: blur(10px)] p-2.5 rounded-md
-           ">
+            <nav className="flex items-center mt-5 bg-[#ffffffb2] [backdrop-filter: blur(10px)] p-2.5 rounded-md">
                 <span className=" text-4xl font-bold mr-2.5">Ankit</span>
-                <ul className="flex">
-                    <li className="m-5">Home</li>
-                    <li className="m-5">Projects</li>
-                    <li className="m-5">Blog</li>
-                    <li className="m-5">About</li>
+                <ul className="relative top-1 flex">
+                    <li className="m-5 cursor-pointer group">
+                        <span>Home</span>
+                        <div className="h-2 scale-x-0 origin-left group-hover:scale-x-100 transition-transform bg-[#FFDB58]"></div>
+                    </li>
+                    <li className="m-5 cursor-pointer group">
+                        <span>Projects</span>
+                        <div className="h-2 scale-x-0 origin-left group-hover:scale-x-100 transition-transform bg-[#FFDB58]"></div>
+                    </li>
+                    <li className="m-5 cursor-pointer group">
+                        <span>About</span>
+                        <div className="h-2 scale-x-0 origin-left group-hover:scale-x-100 transition-transform bg-[#FFDB58]"></div>
+                    </li>
+                    <li className="m-5 cursor-pointer group">
+                        <span>Contact</span>
+                        <div className="h-2 scale-x-0 origin-left group-hover:scale-x-100 transition-transform bg-[#FFDB58]"></div>
+                    </li>
                 </ul>
             </nav>
         </header>
