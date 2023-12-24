a1c3e9825a4918ea990da71a3307542ed2ea117c
Ankit Pal <38988649+palankit003@users.noreply.github.com>
2023-12-24 22:24:57 +0530

    Add: Tech component to Home

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,56 @@
-import Image from 'next/image'
 import Header from './components/Header'
 import Hero from './components/Hero'
+import Tech from './components/Tech'
 
+const Technologies: TechProps[] = [
+  {
+    name: "React", description: "I have a good understanding of ReactJS"
+  },
+  {
+    name: "NextJS", description: "Currently Learning"
+  },
+  {
+    name: "Angular", description: "Currently Learning"
+  },
+  {
+    name: "NestJS", description: "Currently Learning"
+  },
+  {
+    name: "Python", description: "Currently Learning"
+  },
+  {
+    name: "ExpressJS", description: "Currently Learning"
+  },
+  {
+    name: "Fastify", description: "Currently Learning"
+  },
+  {
+    name: "NodeJS", description: "Currently Learning"
+  },
+  {
+    name: "DenoJS", description: "Currently Learning"
+  },
+  {
+    name: "Golang", description: "Currently Learning"
+  },
+  {
+    name: "Rust", description: "Currently Learning"
+  },
+]
 export default function Home() {
   return (
     <>
       <Header />
       <Hero />
+      <div className='grid grid-cols-3 mt-20'>
+        {
+          Technologies.map((item) => {
+            return (
+              <Tech name={item.name} description={item.description} key={item.name} />
+            )
+          })
+        }
+      </div>
     </>
   )
 }
