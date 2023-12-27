20bb78415df8e94ea5076452e7f54fa7762a503e
Ankit Pal <38988649+palankit003@users.noreply.github.com>
2023-12-27 16:14:44 +0530

    Clean: Code cleanup

--- app/page.tsx
+++ app/page.tsx
@@ -2,56 +2,53 @@
 import Header from './components/Header'
 import Hero from './components/Hero'
 import Tech from './components/Tech'
-
-const Technologies: TechProps[] = [
-  {
-    name: "React", description: "I have a good understanding of ReactJS"
-  },
-  {
-    name: "NextJS", description: "Currently Learning"
-  },
-  {
-    name: "Angular", description: "Currently Learning"
-  },
-  {
-    name: "NestJS", description: "Currently Learning"
-  },
-  {
-    name: "Python", description: "Currently Learning"
-  },
-  {
-    name: "ExpressJS", description: "Currently Learning"
-  },
-  {
-    name: "Fastify", description: "Currently Learning"
-  },
-  {
-    name: "NodeJS", description: "Currently Learning"
-  },
-  {
-    name: "DenoJS", description: "Currently Learning"
-  },
-  {
-    name: "Golang", description: "Currently Learning"
-  },
-  {
-    name: "Rust", description: "Currently Learning"
-  },
-]
+import { FrontEndTechnologies, BackEndTechnologies, DatabaseTechnologies, LanguagesTechnologies } from './utils/data'
 export default function Home() {
   return (
     <>
       <Header />
       <Hero />
-
-      <div className='grid lg:grid-cols-5 md:grid-cols-3 mx-20 mt-20 gap-1'>
-        {
-          Technologies.map((item) => {
-            return (
-              <Tech name={item.name} description={item.description} key={item.name} />
-            )
-          })
-        }
+      <div className='lg:px-20 pt-20 px-10 bg-red-400'>
+        <h1 className='text-4xl my-4'>Frontend</h1>
+        <div className='grid grid-cols-2 lg:grid-cols-5 md:grid-cols-3 gap-1'>
+          {
+            FrontEndTechnologies.map((item) => {
+              return (
+                <Tech name={item.name} description={item.description} key={item.name} />
+              )
+            })
+          }
+        </div>
+        <h1 className='text-4xl my-4'>Backend</h1>
+        <div className='grid grid-cols-2 lg:grid-cols-5 md:grid-cols-3 gap-1'>
+          {
+            BackEndTechnologies.map((item) => {
+              return (
+                <Tech name={item.name} description={item.description} key={item.name} />
+              )
+            })
+          }
+        </div>
+        <h1 className='text-4xl my-4'>Database</h1>
+        <div className='grid grid-cols-2 lg:grid-cols-5 md:grid-cols-3 gap-1'>
+          {
+            DatabaseTechnologies.map((item) => {
+              return (
+                <Tech name={item.name} description={item.description} key={item.name} />
+              )
+            })
+          }
+        </div>
+        <h1 className='text-4xl my-4'>Languages</h1>
+        <div className='grid grid-cols-2 lg:grid-cols-5 md:grid-cols-3 gap-1'>
+          {
+            LanguagesTechnologies.map((item) => {
+              return (
+                <Tech name={item.name} description={item.description} key={item.name} />
+              )
+            })
+          }
+        </div>
       </div>
     </>
   )
---
+++ app/utils/data.ts
@@ -0,0 +1,59 @@
+export const FrontEndTechnologies: TechProps[] = [
+    {
+        name: "React", description: "I have a good understanding of ReactJS"
+    },
+    {
+        name: "NextJS", description: "Currently Learning"
+    },
+    {
+        name: "Angular", description: "Currently Learning"
+    },
+]
+export const BackEndTechnologies: TechProps[] = [
+    {
+        name: "NestJS", description: "Currently Learning"
+    },
+    {
+        name: "Django", description: "Currently Learning"
+    },
+    {
+        name: "ExpressJS", description: "Currently Learning"
+    },
+    {
+        name: "Fastify", description: "Currently Learning"
+    },
+    {
+        name: "NodeJS", description: "Currently Learning"
+    },
+    {
+        name: "DenoJS", description: "Currently Learning"
+    },
+
+]
+
+export const DatabaseTechnologies: TechProps[] = [
+    {
+        name: "MongoDB", description: "Currently Learning"
+    },
+    {
+        name: "Postgres", description: "Currently Learning"
+    }
+]
+
+export const LanguagesTechnologies: TechProps[] = [
+    {
+        name: "TypeScript", description: "Currently Learning"
+    },
+    {
+        name: "JavaScript", description: "Currently Learning"
+    },
+    {
+        name: "Python", description: "Currently Learning"
+    },
+    {
+        name: "Golang", description: "Currently Learning"
+    },
+    {
+        name: "Rust", description: "Currently Learning"
+    },
+]
