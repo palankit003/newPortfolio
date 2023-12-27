865f61ef808e065962dff0ab2908df2f6901d723
Ankit Pal <38988649+palankit003@users.noreply.github.com>
2023-12-27 16:13:12 +0530

    WIP: made navbar responsive

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,7 +1,7 @@
 export default function Header() {
     return (
-        <header className="flex w-full justify-center fixed top-3">
-            <nav className="select-none flex items-center mt-5 bg-[rgba(255,255,255,0.1)] backdrop-blur-[2px] p-2.5 rounded-md">
+        <header className="flex w-full justify-center z-50 fixed top-3 ">
+            <nav className=" hidden select-none lg:flex items-center mt-5 bg-[rgba(255,255,255,0.1)] backdrop-blur-[2px] px-5 rounded-full">
                 <span className=" text-4xl font-bold mr-2.5">Ankit</span>
                 <ul className="relative top-1 flex">
                     <li className="m-5 cursor-pointer group">
@@ -21,6 +21,14 @@ export default function Header() {
                         <div className="h-2 scale-x-0 origin-left group-hover:scale-x-100 transition-transform bg-[#FFDB58]"></div>
                     </li>
                 </ul>
+            </nav>
+            <div className="flex flex-col justify-around absolute right-4 w-12 h-12 bg-[#ffffff31] rounded-lg py-2 items-center lg:hidden">
+                <div className=" w-[80%] h-1 bg-gray-300 rounded-lg"></div>
+                <div className=" w-[80%] h-1 bg-gray-300 rounded-lg"></div>
+                <div className=" w-[80%] h-1 bg-gray-300 rounded-lg"></div>
+            </div>
+            <nav className=" lg:hidden">
+
             </nav>
         </header>
     )
